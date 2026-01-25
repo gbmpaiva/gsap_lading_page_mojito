c452af2537721dceccdd322337c33c676e54d2e1
Gabriel Paiva <gabrielmpaiva0703@gmail.com>
2026-01-24 22:20:16 -0300

    Implementação da Navbar e Navtwwe para garantir melhor exp

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import SplitText from "gsap/SplitText";
+import Navbar from "./components/Navbar.jsx";
 
 gsap.registerPlugin(ScrollTrigger,SplitText);
 
 
 const App = () => {
     return (
-        <div className="flex-center h-[100vh]">
-            <h1 className="text-3xl text-indigo-300">Hello oiiii</h1>
-        </div>
+        <main>
+            <Navbar></Navbar>
+        </main>
 
 
     )
---
+++ src/consts/index.js
@@ -0,0 +1,183 @@
+const navLinks = [
+    {
+        id: "cocktails",
+        title: "Coquetéis",
+    },
+    {
+        id: "about",
+        title: "Sobre Nós",
+    },
+    {
+        id: "work",
+        title: "Arte",
+    },
+    {
+        id: "contact",
+        title: "Contato",
+    },
+];
+
+const cocktailLists = [
+    {
+        name: "Chapel Hill Shiraz",
+        country: "AU",
+        detail: "Taça",
+        price: "$10",
+    },
+    {
+        name: "Caten Malbee",
+        country: "AU",
+        detail: "Taça",
+        price: "$49",
+    },
+    {
+        name: "Rhino Pale Ale",
+        country: "CA",
+        detail: "750 ml",
+        price: "$20",
+    },
+    {
+        name: "Guinness Irlandesa",
+        country: "IE",
+        detail: "600 ml",
+        price: "$29",
+    },
+];
+
+const mockTailLists = [
+    {
+        name: "Florescimento Tropical",
+        country: "US",
+        detail: "Taça",
+        price: "$10",
+    },
+    {
+        name: "Maracujá com Hortelã",
+        country: "US",
+        detail: "Taça",
+        price: "$49",
+    },
+    {
+        name: "Brilho Cítrico",
+        country: "CA",
+        detail: "750 ml",
+        price: "$20",
+    },
+    {
+        name: "Espuma de Lavanda",
+        country: "IE",
+        detail: "600 ml",
+        price: "$29",
+    },
+];
+
+const profileLists = [
+    {
+        imgPath: "/images/profile1.png",
+    },
+    {
+        imgPath: "/images/profile2.png",
+    },
+    {
+        imgPath: "/images/profile3.png",
+    },
+    {
+        imgPath: "/images/profile4.png",
+    },
+];
+
+const featureLists = [
+    "Misturas perfeitamente equilibradas",
+    "Finalização impecável",
+    "Sempre gelado",
+    "Habilmente batido e mexido",
+];
+
+const goodLists = [
+    "Ingredientes selecionados à mão",
+    "Técnicas exclusivas",
+    "A arte da coquetelaria em ação",
+    "Sabores frescos macerados na hora",
+];
+
+const storeInfo = {
+    heading: "Onde Nos Encontrar",
+    address: "456, Raq Blvd. #404, Los Angeles, CA 90210",
+    contact: {
+        phone: "(555) 987-6543",
+        email: "hello@jsmcocktail.com",
+    },
+};
+
+const openingHours = [
+    { day: "Seg–Qui", time: "11:00 – 00:00" },
+    { day: "Sex", time: "11:00 – 02:00" },
+    { day: "Sáb", time: "09:00 – 02:00" },
+    { day: "Dom", time: "09:00 – 01:00" },
+];
+
+const socials = [
+    {
+        name: "Instagram",
+        icon: "/images/insta.png",
+        url: "#",
+    },
+    {
+        name: "X (Twitter)",
+        icon: "/images/x.png",
+        url: "#",
+    },
+    {
+        name: "Facebook",
+        icon: "/images/fb.png",
+        url: "#",
+    },
+];
+
+const sliderLists = [
+    {
+        id: 1,
+        name: "Mojito Clássico",
+        image: "/images/drink1.png",
+        title: "Ingredientes Simples, Sabor Marcante",
+        description:
+            "Feita com tequila, suco de limão e licor de laranja, a Margarita é fácil de preparar e cheia de personalidade. Adicione uma borda de sal para o drink perfeito nas noites de verão.",
+    },
+    {
+        id: 2,
+        name: "Mojito de Framboesa",
+        image: "/images/drink2.png",
+        title: "Um Clássico Refrescante que Nunca Falha",
+        description:
+            "A Margarita é um clássico que equilibra o azedo do limão, a suavidade da tequila e um toque de doçura. Batida, frozen ou com gelo — sempre refrescante.",
+    },
+    {
+        id: 3,
+        name: "Brisa Violeta",
+        image: "/images/drink3.png",
+        title: "Ingredientes Simples, Sabor Marcante",
+        description:
+            "Feita com tequila, suco de limão e licor de laranja, a Margarita é fácil de preparar e cheia de personalidade. Adicione uma borda de sal para o drink perfeito nas noites de verão.",
+    },
+    {
+        id: 4,
+        name: "Mojito de Curaçao",
+        image: "/images/drink4.png",
+        title: "Preparado com Cuidado, Servido com Amor",
+        description:
+            "Cada coquetel é feito com ingredientes frescos e paixão por aperfeiçoar cada dose, seja para comemorar ou simplesmente relaxar.",
+    },
+];
+
+export {
+    navLinks,
+    cocktailLists,
+    mockTailLists,
+    profileLists,
+    featureLists,
+    goodLists,
+    openingHours,
+    storeInfo,
+    socials,
+    sliderLists,
+};
