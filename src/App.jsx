e2fceccb39af3f8cecb421c4c4d53912b1eb2b3e
Gabriel Paiva <gabrielmpaiva0703@gmail.com>
2026-01-27 22:57:07 -0300

    Ultimas Funções

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,30 @@
+import React from "react";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import SplitText from "gsap/SplitText";
-import Navbar from './components/Navbar.jsx'
-import Hero from './components/Hero.jsx'
-import Cocktails from './components/Cocktails.jsx'
-import About from './components/About.jsx'
-import Art from './components/Art.jsx'
-import Menu from './components/Menu.jsx'
-import Contact from './components/Contact.jsx'
 
-gsap.registerPlugin(ScrollTrigger,SplitText);
+import Navbar from "./components/Navbar.jsx";
+import Hero from "./components/Hero.jsx";
+import Cocktails from "./components/Cocktails.jsx";
+import About from "./components/About.jsx";
+import Art from "./components/Art.jsx";
+import Contact from "./components/Contact.jsx";
 
+// Registro global dos plugins GSAP
+gsap.registerPlugin(ScrollTrigger, SplitText);
 
 const App = () => {
-    return (
-        <main>
-	 <Navbar />
-	 <Hero />
-	 <Cocktails />
-	 <About />
-	 <Art />
-	 <Menu />
-	 <Contact />
-        </main>
+  return (
+    <main>
+      <Navbar />
+      <Hero />
+      <Cocktails />
+      <About />
+      <Art />
+      <About />
+	  <Contact />
+    </main>
+  );
+};
 
-
-    )
-}
-export default App
+export default App;
